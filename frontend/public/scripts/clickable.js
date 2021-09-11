95cdff965de9e12a4db685e9697d36c070fda71c
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-09-12 00:23:31 +0200

    :recycle: refactor

--- frontend/public/scripts/clickable.js
+++ frontend/public/scripts/clickable.js
@@ -2,6 +2,8 @@
  * switch click is in switch_logic.js
  */
 
+// const { map } = require("async");
+
 
 // todo check current selected
 
@@ -33,99 +35,88 @@ function pre(type) {
 }
 
 function line_clicked(line_id) {
- 
-    if (! pre(clickable.LINE)) {
-        return;
-    }
+    (async () => {
 
-    let main = document.querySelector("body > div > div > main > div > div > div.grid.gap-6.mb-8.md\\:grid-cols-2.xl\\:grid-cols-4");
-    main.innerHTML = "";
+        let curr = clickable.LINE;
+        let mapper = {
+            "Aktivna snaga na početku voda [MW]": "1" + String(line_id) + ";0",
+            "Jalova snaga na početku voda [MVar]": "1" + String(line_id) + ";1",
+            "Aktivna snaga na kraju voda [MW]": "1" + String(line_id) + ";2",
+            "Jalova snaga na kraju voda [MVar]": "1" + String(line_id) + ";3", 
+            "Opterećenje [%]": "1" + String(line_id) + ";4",
+        }; 
 
-    current_selected = clickable.LINE;
-    
-    document.querySelector(".valsandg").style.visibility = "visible";
+        click_action_general(curr, mapper);
 
-    let categoryTemplate = document.querySelector('#card-template');
+    })();
 
-    let title, val, category;
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Aktivna snaga na početku voda [MW]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val 1";
-    main.appendChild(category);
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Jalova snaga na početku voda [MVar]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val 1";
-    main.appendChild(category);
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Aktivna snaga na kraju voda [MW]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val 1";
-    main.appendChild(category);
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Jalova snaga na kraju voda [MVar]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val 1";
-    main.appendChild(category);
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Opterećenje [%]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val 1";
-    main.appendChild(category);
 }
 
 function bus_clicked(bus_id) {
+    (async () => {
 
-    if (! pre(clickable.BUS)) {
+        let curr = clickable.BUS;
+        let mapper = {
+            "Aktivna snaga [MW]": String(bus_id) + ";0",
+            "Jalova snaga [MVar]": String(bus_id) + ";1",
+        };
+
+        click_action_general(curr, mapper);
+
+        let main2 = document.querySelector("body > div > div > main > div > div > div.tmp");
+        main2.innerHTML = "";
+        let graph_template = document.querySelector('#chart-template');
+        let graph = graph_template.content.cloneNode(true);        
+        main2.appendChild(graph);
+        plot_graph();
+
+    })();
+
+}
+
+function trafo_clicked() {
+    (async () => {
+        let curr = clickable.TRANSFROMATOR;
+        let mapper = {
+            "Aktivna snaga na strani s višim naponom [MW]": "20;0",
+            "Jalova snaga na strani s višim naponom [MVar]": "20;1",
+            "Aktivna snaga na strani s nižim naponom [MW]": "20;2",
+            "Jalova snaga na strani s nižim naponom [MVar]": "20;3",
+            "Opterećenje [%]": "20;4"
+        };
+    
+        click_action_general(curr, mapper);
+    })();
+}
+
+async function click_action_general(curr, mapper) {
+
+    if (! pre(curr)) {
         return;
     }
 
     let main = document.querySelector("body > div > div > main > div > div > div.grid.gap-6.mb-8.md\\:grid-cols-2.xl\\:grid-cols-4");
-    let main2 = document.querySelector("body > div > div > main > div > div > div.tmp");
-
     main.innerHTML = "";
-    main2.innerHTML = "";
     
-    current_selected = clickable.BUS;
-    current_id = bus_id;
+    current_selected = curr;
     
     document.querySelector(".valsandg").style.visibility = "visible";
 
     let categoryTemplate = document.querySelector('#card-template');
-    let graph_template = document.querySelector('#chart-template');
 
-    let graph;
+    for (const [key, value] of Object.entries(mapper)) {
+    
+        let category = categoryTemplate.content.cloneNode(true);
+        let title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
+        title.textContent = key;
+        let val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
+        val.textContent = await get_val(value);
+        val.id = value;
+        main.appendChild(category);
+    
+    }
 
-    let title, val, category;
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Aktivna snaga [MW]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val prvi";
-    main.appendChild(category);
-
-    category = categoryTemplate.content.cloneNode(true);
-    title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-    title.textContent = "Jalova snaga [MVar]";
-    val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-    val.textContent = "val 1";
-    main.appendChild(category);
-
-    graph = graph_template.content.cloneNode(true);        
-    main2.appendChild(graph);
-    plot_graph();
+    reloader(curr);
 
 }
 
@@ -139,57 +130,14 @@ async function get_val(key) {
     return response["value"];            
 }
 
-function trafo_clicked() {
-    (async () => {
-
-        if (! pre(clickable.TRANSFROMATOR)) {
-            return;
-        }
-
-        let main = document.querySelector("body > div > div > main > div > div > div.grid.gap-6.mb-8.md\\:grid-cols-2.xl\\:grid-cols-4");
-        
-        current_selected = clickable.TRANSFROMATOR;
-
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-
-        main.innerHTML = "";
-        
-        document.querySelector(".valsandg").style.visibility = "visible";
-
-        let categoryTemplate = document.querySelector('#card-template');
-
-        let title, val, category;
-
-        let mapper = {
-            "Aktivna snaga na strani s višim naponom [MW]": "20;0",
-            "Jalova snaga na strani s višim naponom [MVar]": "20;1",
-            "Aktivna snaga na strani s nižim naponom [MW]": "20;2",
-            "Jalova snaga na strani s nižim naponom [MVar]": "20;3",
-            "Opterećenje [%]": "20;4"
-        };
-
+async function reloader(currently_selected) {
+    while (current_selected == currently_selected) {
+    
         for (const [key, value] of Object.entries(mapper)) {
-        
-            category = categoryTemplate.content.cloneNode(true);
-            title = category.querySelector("div > div:nth-child(2) > p.mb-2.text-sm.font-medium.text-gray-600.dark\\:text-gray-400")
-            title.textContent = key;
-            val = category.querySelector("div > div:nth-child(2) > p.text-lg.font-semibold.text-gray-700.dark\\:text-gray-200")
-            val.textContent = await get_val(value);
-            val.id = value;
-            main.appendChild(category);
-        
+            document.getElementById(value).innerHTML = await get_val(value);
         }
-    
-        while (current_selected == clickable.TRANSFROMATOR) {
-    
-            for (const [key, value] of Object.entries(mapper)) {
-                document.getElementById(value).innerHTML = await get_val(value);
-            }
-            
-            await delay(1000);
-        }
-
-
-
-    })();
+        
+        await delay(1000);
+    }
 }
+

92e013c5c62d0779a547c6ed79cdcfc26a49924a
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-09-12 19:28:40 +0200

    :recycle: cleanup

--- frontend/public/scripts/switch_logic.js
+++ frontend/public/scripts/switch_logic.js
@@ -12,7 +12,6 @@ function init_switch_drivers(num_of_switches) {
 
     switch_logic(num_of_switches);
     init_switch_states(num_of_switches);
-    init_switch_gui();
 
 }
 
@@ -159,33 +158,6 @@ function init_switch_states(num_of_switches) {
     console.log(switch_states);
 }
 
-let switch_gui_mapping;
-
-function init_switch_gui() {
-    if (switch_gui_mapping === undefined) {
-        switch_gui_mapping = {}
-    } else {
-        return;
-    }
-
-    switch_gui_mapping["0"] = new SwitchGui("M 237 140 L 261 130", 
-    "M 237 140 L 261 140");
-    switch_gui_mapping["1"] = new SwitchGui("M 437 140 L 461 130", 
-    "M 437 140 L 461 140");
-    switch_gui_mapping["2"] = new SwitchGui("M 505 60 L 517 50", 
-    "M 505 60 L 517 60");
-    switch_gui_mapping["3"] = new SwitchGui("M 625 60 L 637 50", 
-    "M 625 60 L 637 60");
-    switch_gui_mapping["4"] = new SwitchGui("M 625 100 L 637 90", 
-    "M 625 100 L 637 100");
-    switch_gui_mapping["5"] = new SwitchGui("M 625 180 L 637 170", 
-    "M 625 180 L 637 180");
-    switch_gui_mapping["6"] = new SwitchGui("M 625 220 L 637 210", 
-    "M 625 220 L 637 220");
-    switch_gui_mapping["7"] = new SwitchGui("M 505 220 L 517 210", 
-    "M 505 220 L 517 220");
-}
-
 function fade(element) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
@@ -209,49 +181,21 @@ function sw_clicked(switch_id) {
         document.querySelector("#success").style.visibility = "hidden";
     }, 2000);
 
-    console.log("switch d", element.getAttribute("d"));
-
     let raw_string = element.getAttribute("d").split(" ");
-    
-    console.log("raw", raw_string);
-
     let last = Number(raw_string.pop());    
+    let reformated = raw_string.join(" ") + " "
 
     switch (switch_states[switch_id]) {
         case switch_status.OPENED:
-
             last += 10;        
-
+            reformated += String(last);
+            element.setAttribute("d", reformated);
             break;
 
         case switch_status.CLOSED:
             last -= 10;
-            break;
-
-        default:
-            console.log("switch error on toggle");
-            break;
-    }
-
-    let reformated = raw_string.join(" ") + " " + String(last);
-
-    console.log("refomrmatedd", reformated);
-
-    switch (switch_states[switch_id]) {
-        case switch_status.OPENED:
-
-            // element.setAttribute("style", switch_gui_mapping[switch_id].closed);
-            // element.setAttribute("d", switch_gui_mapping[switch_id].closed);
+            reformated += String(last);
             element.setAttribute("d", reformated);
-
-        
-
-            break;
-
-        case switch_status.CLOSED:
-            // element.setAttribute("style", switch_gui_mapping[switch_id].opened);
-            element.setAttribute("d", reformated);
-
             break;
 
         default:

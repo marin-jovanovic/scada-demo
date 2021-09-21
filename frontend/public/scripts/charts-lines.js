37063a51efd6973c0fc2e7c97c33fdb97c953318
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-09-21 15:12:01 +0200

    line config

--- frontend/public/scripts/charts-lines.js
+++ frontend/public/scripts/charts-lines.js
@@ -1,9 +1,7 @@
 /**
  * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
  */
-
-
-function getRandomInt(max) {
+ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
@@ -30,200 +28,181 @@ function timestamp_to_sth_readable(timestamp_time) {
 function plot_graph(labels_type) {
   (async () => {
 
-  let payload = [];
-  let data;
-  
-  for (const [key, value] of Object.entries(labels_type)) {
-    console.log();
-    console.log();
-    console.log();
+      let payload = [];
+      let data;
 
-    let raw = value.split(";");
+      let colors_i = 0;
+      let colors = [
+        "#ffcc00", 
+        "#ff66cc", 
+        "#0099ff", 
+        "#009900", 
+        "#3333ff", 
+        "#ff99ff",
+        "#DFFF00",
+        "#FFBF00",
+        "#FF7F50",
+        "#DE3163",
+        "#9FE2BF",
+        "#40E0D0",
+        "#6495ED",
+        "#CCCCFF"
+      ]
 
-    let response = await fetch('http://localhost:3000/historical_vals/'+raw[0]+'/'+raw[1]);
-    response = await response.json();
-  
-    data = response["value"];
+      for (const [key, value] of Object.entries(labels_type)) {
+          console.log();
+          console.log();
+          console.log();
 
-    // let t = [];
-    let current_timestamp = new Date().getTime();
- 
-    let t_d = {};
+          let raw = value.split(";");
 
-    let max_interval_exceded = false;
+          let response = await fetch('http://localhost:3000/historical_vals/' + raw[0] + '/' + raw[1]);
+          response = await response.json();
 
-    console.log("prep data", data);
-    data.reverse().forEach(element => {
- 
-      if (! max_interval_exceded) {
-        let raw = element.split(";");
+          data = response["value"];
 
-        let that_time = Number(raw[1]);
-  
-        let f = timestamp_to_sth_readable(current_timestamp);
-        let s = timestamp_to_sth_readable(that_time);
-  
-        let h_diff = f[0] - s[0];
-        let m_diff = f[1] - s[1];
-        let s_diff = f[2] - s[2];
-          
-        if (h_diff == 0 && m_diff == 0) {
+          // let t = [];
+          let current_timestamp = new Date().getTime();
 
-  
-          if (s_diff > 20) {
+          let t_d = {};
 
-            console.log("more than 20s passed, will not log after this");
-            max_interval_exceded = true;
+          let max_interval_exceded = false;
 
-          } else {
+          console.log("prep data", data);
+          data.reverse().forEach(element => {
 
-            t_d[s_diff] = raw[0];
+              if (!max_interval_exceded) {
+                  let raw = element.split(";");
+
+                  let that_time = Number(raw[1]);
+
+                  let f = timestamp_to_sth_readable(current_timestamp);
+                  let s = timestamp_to_sth_readable(that_time);
+
+                  let h_diff = f[0] - s[0];
+                  let m_diff = f[1] - s[1];
+                  let s_diff = f[2] - s[2];
+
+                  if (h_diff == 0 && m_diff == 0) {
+
+
+                      if (s_diff > 20) {
+
+                          console.log("more than 20s passed, will not log after this");
+                          max_interval_exceded = true;
+
+                      } else {
+
+                          t_d[s_diff] = raw[0];
+
+                      }
+
+                  }
+
+                  // t.push(raw[0]);
+              }
+
+          });
+
+          console.log("poor data", t_d);
+
+          let max_index = -1;
+
+          for (const [key, value] of Object.entries(t_d)) {
+              if (key > max_index) {
+                  max_index = key;
+              }
+          }
+
+          console.log("max index", max_index);
+
+          for (let i = 0; i < 21; i++) {
+
+              if (!(i in t_d)) {
+                  t_d[i] = 0;
+
+                  // for (let j = i; j < 21; j++) {
+
+                  // }
+
+              }
 
           }
- 
-        }
-  
-        // t.push(raw[0]);
-      }
 
-    });
-
-    console.log("poor data", t_d);
-
-    let max_index = -1;
-
-    for (const [key, value] of Object.entries(t_d)) {
-      if (key > max_index) {
-        max_index = key;
-      }
-    }
-
-    console.log("max index", max_index);  
-
-    for (let i = 0; i < 21; i++) {
-
-        if (! (i in t_d)) {
-          t_d[i] = 0;
-
-          // for (let j = i; j < 21; j++) {
-
+          console.log("rich data", t_d);
+          // for (const [key, value] of Object.entries(t_d)) {
+          //   console.log(key, "->", value)
           // }
 
-        }
-
-    }
-
-    console.log("rich data", t_d);
-    // for (const [key, value] of Object.entries(t_d)) {
-    //   console.log(key, "->", value)
-    // }
-
-    let t = [];
-    for (const [key, value] of Object.entries(t_d)) {
-      t.push(value);
-    }
+          let t = [];
+          for (const [key, value] of Object.entries(t_d)) {
+              t.push(value);
+          }
 
 
 
-    payload.push(
-      {
-        label: key,
-        backgroundColor: '#0694a2',
-        borderColor: '#0694a2',
-        data: t.reverse(),
-        fill: false,
+          payload.push({
+              label: key,
+              // backgroundColor: '#0694a2',
+              backgroundColor: colors[colors_i],
+              // borderColor: '#0694a2',
+              borderColor: colors[colors_i + 1],
+              data: t.reverse(),
+              fill: false,
+          });
+
+          colors_i += 2;
+
       }
-    );
-
-  }
 
 
-  // x axis labels
-  let time_labels = [];
-  for (let i = 1; i < 21; i++) {
-    time_labels.push("-" + (21 - i));
-  }
-  time_labels.push("0")
+      // x axis labels
+      let time_labels = [];
+      for (let i = 1; i < 21; i++) {
+          time_labels.push("-" + (21 - i));
+      }
+      time_labels.push("0")
 
-  const lineConfig = {
-    type: 'line',
-    data: {
-      labels: time_labels,
-      datasets: 
-        payload,
-      
-      // [
-      //   {
-      //     label: labels_type[0],
-      //     /**
-      //      * These colors come from Tailwind CSS palette
-      //      * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-      //      */
-      //     backgroundColor: '#0694a2',
-      //     borderColor: '#0694a2',
-      //     // data: [43, 48, 40, 54, 67, 73, 70],
-  
-      //     data: [43, 48, 40, 54],
-  
-      //     fill: false,
-  
-      //   },
-      //   {
-      //     label: labels_type[1],
-      //     fill: false,
-      //     /**
-      //      * These colors come from Tailwind CSS palette
-      //      * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-      //      */
-      //     backgroundColor: '#7e3af2',
-      //     borderColor: '#7e3af2',
-      //     data: [24, 50, 64, 74, 52, 51, 65],
-      //   },
-      // ],
-    },
-    options: {
-      responsive: true,
-      /**
-       * Default legends are ugly and impossible to style.
-       * See examples in charts.html to add your own legends
-       *  */
-      legend: {
-        display: false,
-      },
-      tooltips: {
-        mode: 'index',
-        intersect: false,
-      },
-      hover: {
-        mode: 'nearest',
-        intersect: true,
-      },
-      scales: {
-        x: {
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Month',
+      const lineConfig = {
+          type: 'line',
+          data: {
+              labels: time_labels,
+              datasets: payload,
           },
-        },
-        y: {
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Value',
+          options: {
+              responsive: true,
+              legend: {
+                  display: false,
+              },
+              tooltips: {
+                  mode: 'index',
+                  intersect: false,
+              },
+              hover: {
+                  mode: 'nearest',
+                  intersect: true,
+              },
+              scales: {
+                  x: {
+                      display: true,
+                      scaleLabel: {
+                          display: true,
+                          labelString: 'Month',
+                      },
+                  },
+                  y: {
+                      display: true,
+                      scaleLabel: {
+                          display: true,
+                          labelString: 'Value',
+                      },
+                  },
+              },
           },
-        },
-      },
-    },
-  }
+      }
 
+      const lineCtx = document.getElementById("line");
+      window.myLine = new Chart(lineCtx, lineConfig);
 
-  
-
-  // change this to the id of your chart element in HMTL
-  const lineCtx = document.getElementById("line");
-  // console.log("line", lineCtsx);
-  window.myLine = new Chart(lineCtx, lineConfig);
-
-})();
+  })();
 }
